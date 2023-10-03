990b5cbfb10fcc0984d130f1b44352fd5656c144
Vikrant Kumar <133192786+puzzulehead@users.noreply.github.com>
2023-10-03 17:28:01 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,10 +11,26 @@ let valueInNumber = Number(score)//this format guarantees that the type of score
 //true => 1 false => 0
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 //1 => true; 0=> false
 //"" => false
 //"vikrant" => true
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log( typeof stringNumber);
+// console.log( typeof stringNumber);
+
+//**************************Operations*********************************//
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+// console.log(2/3);//in javascript we dont have to use float it will itself give the output in decimal
+let str1 = "hello"
+let str2 = " VMT"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1+ "2");
+console.log(1 + "2" + "3");
+console.log(1 + 2 + "3");
+console.log("1" + 2 + 2);
