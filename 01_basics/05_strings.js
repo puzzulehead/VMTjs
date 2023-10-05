de65797bbe2f086909cb81ad373d07473f06d169
Vikrant Kumar <133192786+puzzulehead@users.noreply.github.com>
2023-10-05 19:27:24 +0000

    playing with strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+//String Interpolation => a modern way to write strings without using plus and double quotes to concatinate values of different varibles
+const name = "vikrant"
+const age = 18
+console.log(`hi this is ${name} and i am ${age}  years old`);
+
+//***************************************************** */
+//Way of accesing (key value pairs)
+//in the name variable the assigned value is vikrant and at key 0 the value is v, at key 1 the value is i 
+//this resembles to array but its not array
+// console.log(name[0]);
+// //using various prototype functions
+// console.log(name.length);
+// console.log(name.toUpperCase());
+console.log(name.charAt('5'));//to find the character at a particular index/position
+console.log(name.indexOf('r'));//to find the position of a particular character
