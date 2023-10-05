76ea53fd719efac4545f9d777177cc652d21c739
Vikrant Kumar <133192786+puzzulehead@users.noreply.github.com>
2023-10-05 18:59:21 +0000

    stack and heap

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -18,7 +18,7 @@ let temperature = null//output = empty
 //symbol =>unique
 
 //object
-console.log(typeof "vikrant");
+// console.log(typeof "vikrant");
 //typeof null is object
 //typeof undefined is undefined
 
@@ -28,14 +28,14 @@ console.log(typeof "vikrant");
 //7 types : Strings,Number,Boolean,null,undefined,Symbol,BigInt
 //Reference (non primitive)
 //Arrays,Objects,Functions
- console.log(2>=0);
+//  console.log(2>=0);
  const heros = ["iron man", "john wick", "sherlock holmes"];
  let myObj = {
     name : "Vikrant",
     age : 18,
  }
 const myFunction = function(){
-    console.log("hello VMT");
+    // console.log("hello VMT");
 }
 //typeof
 //Undefined => undefined
@@ -43,3 +43,31 @@ const myFunction = function(){
 //Boolean => boolean
 //Number =>number
 //String => string
+
+
+//+++++++++++++++++++++++++++++++++++++++//
+//stack and heap memory
+//stack(primitive)   heap (non-primtive)
+//STACK => we get a copy of the original value from stack memory and the original memory remain unchanged
+//HEAP => we get the original reference from the heap memory so during any change the original value is compromised
+let myName = "vikrant"
+let myGameName = myName
+myGameName = "zicka"
+//here in varible myName vikrant is added and then the varible value is assigned to variable myGameName thereafter myGameName value got assigned as zicka
+
+
+
+console.log(myName);
+console.log(myGameName);
+let user = {
+    age : 18,
+    hieght : 6
+
+}
+let anotherUser = user
+anotherUser.age = 19
+console.log(user)
+console.log(anotherUser);
+//object is added in the heap memory
+//so the original is changes in case of any potential change in the value
+
