e0f6d0a813df43c9cdc1758b62ed71baffa3bdc2
Vikrant Kumar <133192786+puzzulehead@users.noreply.github.com>
2024-01-11 14:21:47 +0000

    some strings

--- 01_basics/cohort02.js
+++ 01_basics/cohort02.js
@@ -10,14 +10,76 @@ the code will not run and it will give a error.
 ---->js is a single threaded language which means it can only use one 
 core at a time while running the code and java is a mutltithreaded language.
 */
-let answer=0;
-for(let i=0;i<=100;i++){
-    answer=answer+i
-}
-const ages=[21,22,23,24];
-for(let i=0;i<ages.length;i++){
-    if(ages[i]%2==0){
-        console.log(ages[i]);
-    }
-}
+// let answer=0;
+// for(let i=0;i<=100;i++){
+//     answer=answer+i
+// }
+// const ages=[21,22,23,24];
+// for(let i=0;i<ages.length;i++){
+//     if(ages[i]%2==0){
+//         console.log(ages[i]);
+//     }
+// }
+// //objects
+// const user1 = {
+//     firstName:"vikrant",
+//     gender:"male",
+//     age:"20"
+// }
+// //calling elements
+// console.log(user1["firstaName"])
+
+// //array of objects
+// const alluser =[{
+//     firstName:"vikrant",
+//     gender:"male",
+//     age:"20"
+// },{
+//     firstName:"vik",
+//     gender:male,
+//     age:20
+// },{
+//     firstName:"krishna",
+//     gender:male,
+//     age:20
+
+// },{
+//     firstName:"radha",
+//     gender:female,
+//     age:20
+// }
+// ]
+
+// function operations(a,b,type) {
+//     if(type ==="sum"){
+//         return a+b;
+//     }
+//     if(type ==="minus"){
+//         return a-b;
+//     }
+//     const value = operations(1,2,"sum");
+//     console.log(value);
+    
+// }
+
+/*----->string length*/
+// const str = "vikrant kumar";
+// console.log(str.length);
+// function getLength(str){
+//     console.log("original string:",str);
+//     console.log("length:",str.length);
+// }
+// getLength("hello world");
+
+
+// function mystr(str,target){
+
+//     console.log("actual string",str);
+//     console.log("length:",str.lastIndexOf(target));
+// }
+// mystr("this is me and this is vikrant kumar","is")
+
+
+
+
 
